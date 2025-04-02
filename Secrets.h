dffaa9372edcc351ced4d326ea5e2204afcd502e
rab85 <renvanberkel@yahoo.com>
2025-04-02 20:10:16 +0200

    Add files via upload

---
+++ Secrets.h
@@ -0,0 +1,7 @@
+// wifi settings
+
+const char* ssid = "SID";      // CHANGE IT
+const char* password = "PASSWORD";  // CHANGE IT
+
+// API token for Tibber demo
+const String tibber_accces_token = "TIBBER_ACCESS_TOKEN";

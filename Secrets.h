370a226664e226c99d88e397e76469cbe39c6fd2
rab85 <renvanberkel@yahoo.com>
2025-07-12 15:16:08 +0200

    Update Secrets.h

--- Secrets.h
+++ Secrets.h
@@ -5,3 +5,8 @@ const char* password = "PASSWORD";  // CHANGE IT
 
 // API token for Tibber demo
 const String tibber_accces_token = "TIBBER_ACCESS_TOKEN";
+
+
+// thingspeakinfo
+unsigned long thingspeakChannelNumber = 0;
+const char * thingspeakWriteAPIKey = "your writeable api key";
